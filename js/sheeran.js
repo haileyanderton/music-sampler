2e07c49f40d7d148ac53eec21a29fb22cfb899ae
haileyanderton@gmail.com <hka122095!>
2016-11-11 11:04:44 -0500

    completed assignment files

---
+++ js/sheeran.js
@@ -0,0 +1,44 @@
+// jQuery hide()/show() example with a switch statement
+
+$(document).ready(function() {
+
+      $('.songInfo').hide();
+
+      $('.albumCover').click(function() {
+        $('.songInfo').slideToggle();
+      });
+
+    // function to hide all divs
+    function hideAll() {
+      $('#loveInfo').hide();
+      $('#ynmmidnyInfo').hide();
+      $('#loudInfo').hide();
+      $('#backInfo').hide();
+    }
+
+    hideAll();
+
+    $('.albumCover').click(function() {
+
+
+    hideAll();
+
+  // switch statement
+
+      switch ($(this).attr("id")) {
+        case "loveCover":
+          $('#loveInfo').show();
+          break;
+        case "ynmmidnyCover":
+          $('#ynmmidnyInfo').show();
+          break;
+        case "loudCover":
+          $('#loudInfo').show();
+          break;
+        case "backCover":
+          $('#backInfo').show();
+          break;
+      }
+    });
+
+});
